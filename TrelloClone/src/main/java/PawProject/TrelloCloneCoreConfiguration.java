a7094418ca680f636849238e6c80362a94d2087b
Pawel Szopa <pawel.szopa@glase.se>
2018-10-21 20:16:38 +0200

    Init

---
+++ TrelloClone/src/main/java/PawProject/TrelloCloneCoreConfiguration.java
@@ -0,0 +1,10 @@
+package PawProject;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class TrelloCloneCoreConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
