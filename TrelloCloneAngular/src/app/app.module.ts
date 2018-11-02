104e66642acc1360d08c0c6228819e718611cfae
Gutek <gucio332@gmail.com>
2018-11-02 23:16:59 +0100

    Adding HttpClient for future work :)

--- TrelloCloneAngular/src/app/app.module.ts
+++ TrelloCloneAngular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
@@ -21,6 +22,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {path:'',component: HomeComponent},
       {path:'board',component: BoardComponent},
