be8ae9f3a87e32ad14e52777dcfcbf2b2dc1fbc4
Gutek <gucio332@gmail.com>
2018-12-19 14:27:20 +0100

    Gets for lists and cards. TODO: buttons

---
+++ TrelloCloneAngular/src/app/card.ts
@@ -0,0 +1,10 @@
+export class Card {
+
+    public id: number;
+    public listId: number;
+    public name: string;
+    public desc: string;
+    public position: string;
+    public visivility: string;
+
+}
